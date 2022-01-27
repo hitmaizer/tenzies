3a28ef88765038b10bed9ef377abb816ed26eaeb
hitmaizer <jose.sousalves@gmail.com>
2022-01-27 18:37:53 +0000

    array of objects to have isHold property

--- src/App.js
+++ src/App.js
@@ -4,24 +4,34 @@ import Die from "./components/Die"
 export default function App() {
 
     const [allDices, setAllDices] = React.useState(allNewDice())
-    const dieElements = allDices.map(dice => <Die value={dice} />)
-
+    
     function allNewDice(){
         const newDice = []
-        for (let i = 0; i < 10; i++){
-            newDice.push(Math.ceil(Math.random() * 6))
-        } 
+
+        for (let i = 0; i < 10; i++) {
+            newDice.push({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false
+            })
+        }
         return newDice
     }
 
-   
+    const diceElements = allDices.map(die => <Die value={die.value} />)
+
+    function rollNewDices() {
+        setAllDices(allNewDice())
+    }
 
     return (
         <div className="page--wrapper">
             <main>
                 <div className="die--container">
-                   {dieElements}
+                   {diceElements}
                 </div> 
+                <div className="btn--section">
+                    <button className="page--button" onClick={rollNewDices}> Roll </button>
+                </div>
             </main>
         </div>
     )
