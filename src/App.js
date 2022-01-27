de95e498c904bf063a7a31ce37fe0f0ea0fb62e5
hitmaizer <jose.sousalves@gmail.com>
2022-01-27 19:45:06 +0000

    holdDice function working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Die from "./components/Die"
+import {nanoid} from "nanoid"
 
 export default function App() {
 
@@ -11,18 +12,33 @@ export default function App() {
         for (let i = 0; i < 10; i++) {
             newDice.push({
                 value: Math.ceil(Math.random() * 6), 
-                isHeld: false
+                isHeld: false,
+                id: nanoid()
             })
         }
         return newDice
     }
 
-    const diceElements = allDices.map(die => <Die value={die.value} />)
+    const diceElements = allDices.map(die => (
+        <Die key={die.id}  
+        value={die.value} 
+        hold={die.isHeld}
+        holdDice={() => holdDice(die.id)}
+        
+        />
+    ))
 
     function rollNewDices() {
         setAllDices(allNewDice())
     }
 
+    function holdDice(id) {
+        setAllDices(oldNotes => oldNotes.map(note => {
+            return id === note.id ? {...note, isHeld: !note.isHeld} : note
+        }))    
+    }
+    
+    console.log(allDices)
     return (
         <div className="page--wrapper">
             <main>
